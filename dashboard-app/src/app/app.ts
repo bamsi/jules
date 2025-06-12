efbcd51c1a83a560fb68b4f97864dd5a74dc2818
Haji Bamsi <hibamsi@gmail.com>
2025-06-12 14:23:52 +0300

    update app

--- dashboard-app/src/app/app.ts
+++ dashboard-app/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationMenu } from './navigation-menu/navigation-menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationMenu],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- dashboard-app/src/app/navigation-menu/navigation-menu.ts
+++ dashboard-app/src/app/navigation-menu/navigation-menu.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-navigation-menu',
   imports: [],
   templateUrl: './navigation-menu.html',
